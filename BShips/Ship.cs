546ce48534142b9fe35dbb2b9ae98b64ba83317b
Clint Kingston <ckingston1@woolworths.com.au>
2023-01-09 05:14:55 +1100

    refactoring of ships class

--- BShips/Ship.cs
+++ BShips/Ship.cs
@@ -8,7 +8,8 @@ public class Ship
     public readonly int Length;
     public readonly Orientation Orientation;
     public readonly Point [] XyArray;
-    public readonly bool IsSunk = false;
+    public  bool IsSunk = false;
+    public int HitCount;
     public readonly string Symbol;
 
     public Ship(ShipName shipName, List<Point> occupiedCells)
@@ -16,14 +17,19 @@ public class Ship
         var shipLengths = new[] { 5, 4, 3, 3, 2 };
         var length = shipLengths[(int)shipName];
         var xyArray = new Point[length];
+        var hitCount = 0;
+        var isSunk = false;
         ShipName = shipName;
         Length = length;
+        HitCount = hitCount;
+        IsSunk = isSunk;
         Orientation = GetOrientation();
         XyArray = xyArray;
         Symbol = shipName.ToString()[..1];
         PlaceShip(xyArray, occupiedCells);
     }
 
+    // Returns an Orientation at random of either Horizontal or vertical
     private static Orientation GetOrientation()
     {
         var rnd = new Random(Guid.NewGuid().GetHashCode());
@@ -31,6 +37,7 @@ public class Ship
         return (Orientation)orientation;
     }
 
+    //Returns a random X,Y Point.
     private static Point GetRandomCoordinate()
     {
         var rnd = new Random(Guid.NewGuid().GetHashCode());
@@ -42,6 +49,7 @@ public class Ship
         return xy;
     }
 
+    
     private void PlaceShip(Point[] xyArray, List<Point> occupiedCells)
     {
         do
@@ -55,19 +63,14 @@ public class Ship
                     while (!canBePlaced)
                     {
                         xyArray[0] = GetRandomCoordinate();
-                        if (xyArray[0].X + Length < 10)
-                        {
-                            canBePlaced = true;
-                        }
+                        if (xyArray[0].X + Length < 10) canBePlaced = true;
                     }
-
                     for (var i = 1; i < Length; i++)
                     {
                         xyArray[i].X = xyArray[0].X;
                         xyArray[i].X += i;
                         xyArray[i].Y = xyArray[0].Y;
                     }
-
                     break;
                 }
                 case Orientation.Vertical:
@@ -75,25 +78,21 @@ public class Ship
                     while (!canBePlaced)
                     {
                         xyArray[0] = GetRandomCoordinate();
-                        if (xyArray[0].Y + Length < 10)
-                        {
-                            canBePlaced = true;
-                        }
+                        if (xyArray[0].Y + Length < 10) canBePlaced = true;
                     }
-
                     for (var i = 1; i < Length; i++)
                     {
                         xyArray[i].X = xyArray[0].X;
                         xyArray[i].Y = xyArray[0].Y;
                         xyArray[i].Y += i;
                     }
-
                     break;
                 }
             }
         } while (!IsOverLapping(xyArray, occupiedCells));
     }
 
+    // determines whether the ship placement will overlap any already placed ships
     private static bool IsOverLapping(IEnumerable<Point> checkXyArray, List<Point> checkOccupiedCells)
     {
         var isAlreadyOccupied = checkOccupiedCells.ToArray().Intersect(checkXyArray);
