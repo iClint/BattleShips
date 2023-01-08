34324045b582be2663660e8a5e836df311479f8f
Clint Kingston <ckingston1@woolworths.com.au>
2023-01-09 05:16:23 +1100

    refactoring of userinput

--- BShips/UserInput.cs
+++ BShips/UserInput.cs
@@ -4,14 +4,16 @@ namespace BShips;
 
 public class UserInput
 {
-
     public static Point GetPlayerXy()
     {
+        var cursorLeft = Console.CursorLeft;
+        var cursorTop = Console.CursorTop;
         Console.Write("Target Co-Ordinates: X = ");
         var x = Console.ReadKey();
         Console.Write(", Y = ");
         var y = Console.ReadKey();
-        if (char.IsDigit(x.KeyChar) && char.IsDigit(x.KeyChar))
+        var deleteChars = Console.CursorLeft;
+        if (char.IsDigit(x.KeyChar) && char.IsDigit(y.KeyChar))
         {
             return new Point() 
             { 
@@ -19,15 +21,38 @@ public class UserInput
                 Y = (int.Parse(y.KeyChar.ToString()))
             };
         }
+        
+        ClearLine(cursorLeft,cursorTop);
 
-        Console.WriteLine("\nInvalid Co-Ordinate!\n" + 
-                          "Any key to try again...");
-        var anyKey = Console.Read();
+        Console.Write("Invalid Co-Ordinate!\n");
+        SpaceBarToContinue();
         return Point.Empty;
     }
 
+    // Clear buffer for Cursor position to buffer width.
+    public static void ClearLine(int left, int top)
+    {
+        Console.SetCursorPosition(left, top);
+        for (int i = 0; i < Console.BufferWidth; i++)
+        {
+            Console.Write(" ");
+        }
+        Console.SetCursorPosition(left, top);
+    }
+    
+    // Check received point is valid
     public static bool CheckValidPoint(Point xy)
     { 
-        return (xy.X is >= 0 and <= 9 && xy.Y is >=0 and <=9 ? true : false) ;
+        return xy.X is >= 0 and <= 9 && xy.Y is >=0 and <=9 && !xy.IsEmpty ;
+    }
+
+    // Pause for user interaction.
+    public static void SpaceBarToContinue()
+    {
+        ConsoleKeyInfo anyKey = default;
+        Console.Write("Any Space to continue...");
+        while(anyKey.Key != ConsoleKey.Spacebar) 
+            anyKey = Console.ReadKey(true);
     }
 }
+
