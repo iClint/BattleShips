618c09cd084a182f91520c9d0cb518a008c0f4da
Clint Kingston <ckingston1@woolworths.com.au>
2023-01-09 05:13:25 +1100

    main program current debugging

--- BShips/Program.cs
+++ BShips/Program.cs
@@ -12,12 +12,52 @@ var ui = new UserInput();
 
 //debug();
 
-Fleet.Fleet2Map(playerMap, playerFleet.TheFleet);
-Fleet.Fleet2Map(enemyMap, enemyFleet.TheFleet);
-Map.DrawMap(playerMap);
-Map.DrawMap(playerTracking);
-Map.DrawMap(enemyMap);
-Map.DrawMap(enemyTracking);
+Fleet.Fleet2Map(playerMap, playerFleet.Ships);
+Fleet.Fleet2Map(enemyMap, enemyFleet.Ships);
+// Map.DrawMap(playerMap);
+// Map.DrawMap(playerTracking);
+// Map.DrawMap(enemyMap);
+// Map.DrawMap(enemyTracking);
+while (true)
+{
+    Console.Clear();
+    Map.DrawMap(playerTracking);
+    Map.DrawMap(playerMap);
+    //UserInput.GetPlayerXy();
+    var cursorLeft = Console.CursorLeft;
+    var cursorTop = Console.CursorTop;
+    var xy = UserInput.GetPlayerXy();
+    var result = Map.CheckTargetedXy(xy, playerMap, playerTracking, playerFleet);
+    playerTracking.Grid[xy.X, xy.Y].Targeted = true;
+    if (result.Hit)
+    {
+        Console.Clear();
+        Map.DrawMap(playerTracking);
+        Map.DrawMap(playerMap);
+        Console.WriteLine(
+            playerFleet.Ships[(int)result.Ship!].IsSunk ? "\n         {0} Sunk !!!" : "\n         {0} Hit !!!",
+            result.Ship.ToString());
+        Console.WriteLine(playerTracking.Grid[xy.X, xy.Y].Targeted);
+        Console.WriteLine(playerTracking.Grid[xy.X, xy.Y].Occupied);
+        Console.WriteLine(playerFleet.Ships[(int)result.Ship!].IsSunk);
+        Console.WriteLine(playerFleet.Ships[(int)result.Ship!].HitCount);
+    }
+    if(!result.Hit && !result.IsAlreadyTargeted)
+    {
+        Console.Clear();
+        Map.DrawMap(playerTracking);
+        Map.DrawMap(playerMap);
+        Console.WriteLine("\n             Miss");       
+        Console.WriteLine(playerTracking.Grid[xy.X, xy.Y].Targeted);
+        Console.WriteLine(playerTracking.Grid[xy.X, xy.Y].Occupied);
+    }
+    if(result.IsAlreadyTargeted)
+    {
+        UserInput.ClearLine(cursorLeft,cursorTop);
+        Console.Write("Invalid Co-Ordinate!\n");
+    }
+    UserInput.SpaceBarToContinue();
+}
 var PlayerXyTarget = UserInput.GetPlayerXy();
 var prevPlayerGuesses = new List<Point>() { Capacity = 100 };
 var prevEnemyGuesses = new List<Point>(){ Capacity = 100 };
